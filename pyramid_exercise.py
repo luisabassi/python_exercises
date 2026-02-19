f96a5883b68d3cd48cfbf44382b33ee4dd7e94be
Luísa Bassi <luisachagasbassi@gmail.com>
2026-02-19 14:06:30 -0300

    observation

--- pyramid_exercise.py
+++ pyramid_exercise.py
@@ -8,3 +8,7 @@ while blocks >= layer:
     height += 1
     
 print("The height of the pyramid:", height)
+
+# The code calculates the height of a pyramid based on the number of blocks provided by the user. 
+# It uses a while loop to determine how many layers can be built with the given blocks, incrementing 
+# the layer and height until there are not enough blocks left to build the next layer.
